2c2963443d76ffcccde011101b8bd6d5ebe44a7f
horse <littlehorseboy@gmail.com>
2019-07-31 13:36:40 +0800

    test: AudioContainer

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,8 @@ module.exports = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
   },
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/config/tests/__mocks__/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/config/tests/__mocks__/styleMock.js',
+  },
 };
--- src/components/AudioContainer/AudioContaner.test.tsx
+++ src/components/AudioContainer/AudioContaner.test.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react'; // eslint-disable-line import/no-extraneous-dependencies
+import '@testing-library/jest-dom/extend-expect'; // eslint-disable-line import/no-extraneous-dependencies
 import AudioContainer from './AudioContainer';
 
 describe('AudioContainer.tsx', (): void => {
   afterEach(cleanup);
 
-  test('test AudioContainer', (): void => {
+  test('<audio> tagName === "AUDIO"', (): void => {
     const { getByTestId } = render(<AudioContainer />);
-    expect(getByTestId('root-test').textContent).toBe('test');
+    expect((getByTestId('root-test') as HTMLAudioElement).tagName === 'AUDIO');
+  });
+
+  test('<audio> autoplay === false', (): void => {
+    const { getByTestId } = render(<AudioContainer />);
+    expect((getByTestId('root-test') as HTMLAudioElement).autoplay === false);
+  });
+
+  test('<audio> includes <track kind="captions"', (): void => {
+    const { getByTestId } = render(<AudioContainer />);
+    expect((getByTestId('root-test') as HTMLAudioElement).innerHTML.includes('<track kind="captions"'));
+  });
+
+  test('<audio> toBeInTheDocument', (): void => {
+    const { getByTestId } = render(<AudioContainer />);
+    expect((getByTestId('root-test') as HTMLAudioElement)).toBeInTheDocument();
   });
 });
