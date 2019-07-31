1d4696c9a6500aedf82cfbb68ed2e90f9485a3c7
littlehorseboy <littlehorseboy@gmail.com>
2019-07-31 21:09:05 +0800

    feat: Main.tsx 思考整體排版

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -9,6 +9,12 @@ const useStyles = makeStyles({
   },
   leftPane: {
     flexGrow: 1,
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  playerToolbar: {
+    height: 100,
+    backgroundImage: 'linear-gradient(to top, #361024 0%, #772020 100%)',
   },
 });
 
@@ -18,7 +24,12 @@ export default function Main(): JSX.Element {
   return (
     <div className={classes.root}>
       <div className={classes.leftPane}>
-        <AudioContainer />
+        <div>
+          <AudioContainer />
+        </div>
+        <div className={classes.playerToolbar}>
+
+        </div>
       </div>
 
       <RightPaneToolbar />
