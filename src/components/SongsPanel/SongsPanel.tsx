11fc7baadfca458d6950b441fd0d85e7550c9f03
horse <littlehorseboy@gmail.com>
2019-08-01 15:29:13 +0800

    feat: simplebar 也還不成功

--- src/components/SongsPanel/SongsPanel.tsx
+++ src/components/SongsPanel/SongsPanel.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import SimpleBar from 'simplebar-react';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -7,8 +6,6 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import MusicNoteIcon from '@material-ui/icons/MusicNote';
-
-require('simplebar/dist/simplebar.min.css');
 
 const useStyles = makeStyles({
   songsPanel: {
